512aa2ca4ed051de37231cce4e6cae41c72413bd
unknown <19002369@alumnos.utleon.edu.mx>
2023-06-17 11:05:51 -0500

    BMZ 17/06

---
+++ src/app/grupos/formularios/operas-bas/operas-bas.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  templateUrl: './operas-bas.component.html',
+  styleUrls: ['./operas-bas.component.css']
+})
+export class OperasBasComponent {
+  num1!:number;
+  num2!:number;
+  resultado!:number;
+  operacionSeleccionada:string='suma';
+  tipoOperaciones=[
+    'suma',
+    'res',
+    'multiplicacion',
+    'division',
+  ];
+
+  calcular(){
+    switch(this.operacionSeleccionada){
+      case 'suma':this.resultado=this.num1+this.num2;
+                  break;
+    }
+  }
+}
--- src/app/grupos/iric/iric.component.ts
+++ src/app/grupos/iric/iric.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'app-iric',
@@ -7,28 +8,45 @@ import { Component } from '@angular/core';
 })
 export class IricComponent {
 
+  imageWidth:number=100
+  imageMargin:number=2
+  muestraImg:boolean=true
+  listfilter?:string
+  
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
   alumnosIric:any[]=[
     {
-    matricula:1234,
-    nombre:'Bernardo',
-    edad:22,
-    correo: 'berni@gmail.com',
-    foto:'https://th.bing.com/th/id/OIP.ipe4TTMqKM1f16cRzp2bugHaHa?pid=ImgDet&rs=1'
+      
+        matricula:12344,
+        nombre:'Zamora',
+        edad:8,
+        correo:"Zamora@gmail.com",
+        foto:"https://c.pxhere.com/photos/fb/0a/child_smile_happy_infante_small_face_son_brother-646105.jpg!d"      
     },
     {
-    matricula:1278,
-    nombre:'Itzel',
-    edad:22,
-    correo: 'itzel@gmail.com',
-    foto:'https://th.bing.com/th/id/R.909b0de9a4f5e9db45a827de0b408f9e?rik=vsUfbdfqWa9Zbg&riu=http%3a%2f%2fagencia-fotografia.com%2fwp-content%2fuploads%2f2014%2f06%2ffoto3.jpg&ehk=zDPYSCCnqy4GFXCkGvqXzH2cnaQ2CCXOU6GW8v1vL1o%3d&risl=&pid=ImgRaw&r=0'
-    },
+      
+      matricula:222425,
+      nombre:'Luna',
+      edad:23,
+      correo:"Luna@gmail.com",
+      foto:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+    }
+    ,
     {
-    matricula:1256,
-    nombre:'Marco',
-    edad:22,
-    correo: 'Marco@gmail.com',
-    foto:'https://th.bing.com/th/id/R.5ce936c4a65660653ac3b674fc9f9a36?rik=L%2bIZ%2bOVxOexXXA&riu=http%3a%2f%2ftusimagenesde.com%2fwp-content%2fuploads%2f2014%2f09%2fimagenes-de-personas-1.jpg&ehk=hw2S4IJ5ETZkDn3Apryd8D%2bIZD%2bTl1GSq2pHiDWuquw%3d&risl=&pid=ImgRaw&r=0'
-    },
+      
+      matricula:12345,
+      nombre:'Marco',
+      edad:24,
+      correo:"Marco@gmail.com",
+      foto:'https://th.bing.com/th/id/R.5ce936c4a65660653ac3b674fc9f9a36?rik=L%2bIZ%2bOVxOexXXA&riu=http%3a%2f%2ftusimagenesde.com%2fwp-content%2fuploads%2f2014%2f09%2fimagenes-de-personas-1.jpg&ehk=hw2S4IJ5ETZkDn3Apryd8D%2bIZD%2bTl1GSq2pHiDWuquw%3d&risl=&pid=ImgRaw&r=0'
+    }
   ]
+
+  
+  
+
 
 }
